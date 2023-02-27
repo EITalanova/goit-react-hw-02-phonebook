39b654a38814e5f922bdff53b27e98cd33922a68
EITalanova <elenabelichenko77@gmail.com>
2023-02-26 20:12:50 -0800

    Fixed bug

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,5 @@
 import css from './Filter.module.css';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const Filter = ({ filter, onChange }) => {
